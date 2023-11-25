f447d5852538e1680de0173fd2b079d1e272dc08
balkenic <balkenic@students.zhaw.ch>
2023-11-25 10:05:43 +0100

    bach script

---
+++ piepeline_PPMI_NN.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in{0..5};do
+    python PPMI_prediction_NN.py
+    
+    echo q | python Split_Aggregate.py
+    cd build
+    ./fedavg_aggregation -r 0 -n 100 -d "PPMI"&
+    ./fedavg_aggregation -r 1 -n 100 -d "PPMI"
+    cd ..
+    echo c | python Split_Aggregate.py
+
+    python PPMI_prediction_NN.py blabla
+    echo $i
+done;
