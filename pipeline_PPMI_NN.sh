3f7449c54b4ae4903371cc81fe9e72815af06c19
Matthias von Arx <matthias.von-arx@gmx.ch>
2023-11-27 00:10:09 +0100

    enhanced pipeline with MODE param

--- pipeline_PPMI_NN.sh
+++ pipeline_PPMI_NN.sh
@@ -4,13 +4,26 @@ rm results.txt
 rm model/PPMI/GlobalModel.txt
 python PPMI_prediction_NN.py 2 999  >> results.txt
 
+# Change params here
+##########################################################
+# mode 2 = Q-fed-avg, mode 1 = weighted-avg, mode 0 = normal avg
+MODE=2
+##########################################################
+
+
+if ["$MODE" -eq 2 ]; then
+    SPLIT_STR="q"
+else
+    SPLIT_STR="s"
+fi 
+
 for i in $(seq 1 5);
 do
 
     echo $i >> results.txt
     python PPMI_prediction_NN.py 0 $i
     
-    echo q | python Split_Aggregate.py
+    echo ($SPLIT_STR) | python Split_Aggregate.py
     cd build
     ./fedavg_aggregation -r 0 -n 100 -d "PPMI"&
     ./fedavg_aggregation -r 1 -n 100 -d "PPMI"
