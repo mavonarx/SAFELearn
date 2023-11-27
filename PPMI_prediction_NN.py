2cd50c0c535a719a3c1bec16d9346b746ed87571
balkenic <balkenic@students.zhaw.ch>
2023-11-27 09:16:15 +0100

    increased L

--- PPMI_prediction_NN.py
+++ PPMI_prediction_NN.py
@@ -12,8 +12,8 @@ from torcheval.metrics.functional import multiclass_f1_score, multiclass_auroc
 
 # Change constants here
 ###############################################################################
-MODE = int(sys.argv[1]) # 0 is training mode, 1 is eval mode, 2 is print params mode
-LIPSCHITZCONSTANT = 10
+MODE = int(sys.argv[1])  # 0 is training mode, 1 is eval mode, 2 is print params mode
+LIPSCHITZCONSTANT = 1000  # this should be: 1 / learning_rate (ADAM has learning_rate of e^-3 so we use 1000 as default)
 Q_FACTOR = 1
 TORCHSEED = int(sys.argv[2])
 DEFAULT_DEVICE = "cpu"
