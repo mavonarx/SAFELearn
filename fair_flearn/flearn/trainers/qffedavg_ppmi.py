0b92ef1d4bbe95ac6e7e5421b4f3363be4993dd8
balkenic <balkenic@students.zhaw.ch>
2023-12-05 10:48:41 +0100

    more ravel

--- fair_flearn/flearn/trainers/qffedavg_ppmi.py
+++ fair_flearn/flearn/trainers/qffedavg_ppmi.py
@@ -115,7 +115,7 @@ class Server(BaseFedarated):
             # at this point our arrays are heterogeneous, we need them homogenous and in one arary to work with them in safelearn
             Deltas = np.concatenate((Deltas[0][0].reshape(-1,), Deltas[0][1].reshape(-1,), Deltas[0][2].reshape(-1,), Deltas[0][3].reshape(-1,), Deltas[0][4].reshape(-1,), 
                                      Deltas[0][5].reshape(-1,), Deltas[0][6].reshape(-1,), Deltas[0][7].reshape(-1,), Deltas[0][8].reshape(-1,), Deltas[0][9].reshape(-1,)))
-            weights_before = np.concatenate((weights_before[0].ravel().reshape(-1,), weights_before[0][1].ravel().reshape(-1,), weights_before[0][2].reshape(-1,), weights_before[0][3].reshape(-1,), weights_before[0][4].reshape(-1,), 
+            weights_before = np.concatenate((weights_before[0].ravel().reshape(-1,), weights_before[0][1].ravel().reshape(-1,), weights_before[0][2].ravel().reshape(-1,), weights_before[0][3].ravel().reshape(-1,), weights_before[0][4].reshape(-1,), 
                                             weights_before[0][5].reshape(-1,), weights_before[0][6].reshape(-1,), weights_before[0][7].reshape(-1,), weights_before[0][8].reshape(-1,), weights_before[0][9].reshape(-1,)))
             # estimation of the local Lipchitz constant
             hs.append(self.q * np.float_power(loss+1e-10, (self.q-1)) * norm_grad(grads) + (1.0/self.learning_rate) * np.float_power(loss+1e-10, self.q))
