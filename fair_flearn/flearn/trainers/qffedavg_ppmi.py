ef28745f106c705b100a99a2d4c00595d2aebdaf
balkenic <balkenic@students.zhaw.ch>
2023-12-05 10:39:46 +0100

    zip

--- fair_flearn/flearn/trainers/qffedavg_ppmi.py
+++ fair_flearn/flearn/trainers/qffedavg_ppmi.py
@@ -90,6 +90,7 @@ class Server(BaseFedarated):
         for client_index, c in enumerate(selected_clients):
             Deltas = []
             hs = []
+            weights_before_test =[]
             # communicate the latest model
             c.set_params(self.latest_model)
             weights_before = c.get_params()
@@ -107,6 +108,11 @@ class Server(BaseFedarated):
             grads = [(u - v) * 1.0 / self.learning_rate for u, v in zip(weights_before, new_weights)]
             
             Deltas.append([np.float_power(loss+1e-10, self.q) * grad for grad in grads])
+            weights_before_test.append([u * 1.0 for u in zip(weights_before)])
+            print(weights_before_test.shape, "entire model's shape \n") 
+            print(weights_before_test, "entire model \n") 
+            
+            
             
             # at this point our arrays are heterogeneous, we need them homogenous and in one arary to work with them in safelearn
             Deltas = np.concatenate((Deltas[0][0].reshape(-1,), Deltas[0][1].reshape(-1,), Deltas[0][2].reshape(-1,), Deltas[0][3].reshape(-1,), Deltas[0][4].reshape(-1,), 
@@ -118,8 +124,8 @@ class Server(BaseFedarated):
             combined = np.concatenate((np.array(hs), Deltas))
             np.savetxt(f"{MODEL_PATH}Delta_{client_index}.txt", combined, fmt='%.8f')
 
-        print(weights_before.shape, "entire model's shape \n") 
-        print(weights_before, "entire model \n") 
+        #print(weights_before.shape, "entire model's shape \n") 
+        #print(weights_before, "entire model \n") 
         
         np.savetxt(f"{GLOBAL_MODEL_PATH}", weights_before, fmt='%.8f')
         
