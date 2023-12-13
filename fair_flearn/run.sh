b94ddc62a2ed5afb8d898fcf9f9d4cdaf7ce43e8
balkenic <balkenic@students.zhaw.ch>
2023-12-13 18:13:56 +0100

    run for vehicle

--- fair_flearn/run.sh
+++ fair_flearn/run.sh
@@ -1,14 +1,14 @@
-python3  -u main.py --dataset=ppmi --optimizer=qffedavg_ppmi  \
-            --learning_rate=0.0001 \
+python3  -u main.py --dataset=vehicle --optimizer=qffedavg  \
+            --learning_rate=0.01 \
             --learning_rate_lambda=0.01 \
-            --num_rounds=1 \
+            --num_rounds=20 \
             --eval_every=1 \
-            --clients_per_round=5 \
+            --clients_per_round=10 \
             --batch_size=64 \
-            --q=0.01 \
-            --model='PPMI_TF_NN' \
+            --q=5 \
+            --model='svm' \
             --sampling=1  \
-            --num_epochs=100 \
+            --num_epochs=10 \
             --data_partition_seed=1 \
             --log_interval=10 \
             --static_step_size=0 \
