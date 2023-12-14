c91402a8e3d0d7602bfb68a7806c0591a9cbfd7c
balkenic <balkenic@students.zhaw.ch>
2023-12-14 16:39:39 +0100

    run for sent140

--- fair_flearn/run.sh
+++ fair_flearn/run.sh
@@ -1,15 +1,15 @@
-python3  -u main.py --dataset=vehicle --optimizer=qffedavg  \
-            --learning_rate=0.01 \
+python3  -u main.py --dataset=sent140 --optimizer=qffedavg  \
+            --learning_rate=0.03 \
             --learning_rate_lambda=0.01 \
             --num_rounds=1 \
             --eval_every=1 \
             --clients_per_round=10 \
-            --batch_size=64 \
+            --batch_size=32 \
             --q=1 \
             --model='svm' \
             --sampling=1  \
             --num_epochs=10 \
-            --data_partition_seed=1000 \
+            --data_partition_seed=1 \
             --log_interval=10 \
             --static_step_size=0 \
             --track_individual_accuracy=0 #\
