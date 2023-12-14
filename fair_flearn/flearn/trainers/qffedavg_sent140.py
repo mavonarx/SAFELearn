48888fa035b7689c0be49540f565018960641e81
balkenic <balkenic@students.zhaw.ch>
2023-12-14 18:03:45 +0100

    print removed

--- fair_flearn/flearn/trainers/qffedavg_sent140.py
+++ fair_flearn/flearn/trainers/qffedavg_sent140.py
@@ -113,14 +113,6 @@ class Server(BaseFedarated):
             grads = [(u - v) * 1.0 / self.learning_rate for u, v in zip(weights_before, new_weights)]
             
             Deltas.append([np.float_power(loss+1e-10, self.q) * grad for grad in grads])
-            print(weights_before)
-            print(weights_before[0].shape)
-            print(weights_before[1].shape)
-            print(weights_before[2].shape)
-            print(weights_before[3].shape)
-            print(weights_before[4].shape)
-            print(weights_before[5].shape)
-            print(weights_before[6].shape)
             
             Deltas = np.concatenate((Deltas[0][0].reshape(-1,), Deltas[0][1].reshape(-1,), Deltas[0][2].reshape(-1,), Deltas[0][3].reshape(-1,), Deltas[0][4].reshape(-1,), 
                                      Deltas[0][5].reshape(-1,), Deltas[0][6].reshape(-1,)))
